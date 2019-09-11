2ba2bdb5d9abf5b3e58c941e1bcd2e540eeaff31
insideoutzombie <david@web3devs.com>
2019-09-11 11:47:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let onebtnElement = document.getElementById("onebtn");
+let twobtnElement = document.getElementById("twobtn")
+let titleElement = document.querySelector("title");
+let mainElement = document.querySelector("body");
+
+onebtnElement.addEventListener('click', function() {
+  titleElement.innerText = 1;
+  mainElement.style.backgroundColor = "blue"
+  // mainElement.classList.style.backgroundColor("blueBackground");
+
+})
+
+twobtnElement.addEventListener('click', function() {
+  titleElement.innerText = 2;
+  mainElement.style.backgroundColor = "red"
+})
